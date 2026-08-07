a7e48c7c09ee031ff23010ce174e519039ed25df
Ruben Bañuelos <rubenbanuelos@users.noreply.github.com>
2026-08-07 13:20:33 -0600

    added radar session file and drew background

---
+++ mygame/app/radar/draw.rb
@@ -0,0 +1,30 @@
+#Let's get this Radar simulator to do some actual radaring.
+
+def draw_background args
+  args.outputs.sprites << {
+    x: 0,
+    y: 0,
+    w: 1280,
+    h: 720,
+    path: '/mygame/sprites/background.png'
+  }
+end
+
+def draw_target(args)
+
+  #Draw actual radar target
+  args.outputs.sprites << {
+    x: 640,
+    y: 360,
+    w: 3,
+    h: 3,
+    path: :solid,
+    r: 255,
+    g: 255,
+    b: 0,
+    a: 255
+  }
+
+end
+
+def
---
+++ mygame/app/radar/session.rb
@@ -0,0 +1,9 @@
+require_relative "../app/aircraft/aircraft"
+require_relative "../app/world/waypoint"
+require_relative "../app/world/weather"
+
+class Session
+  #Class containing a radar session
+  def initialize
+  end
+end
