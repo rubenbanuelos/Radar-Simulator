2b56000923ec7f8b08159593ffff6ac60c685f74
Ruben Bañuelos <rubenbanuelos@users.noreply.github.com>
2026-08-06 23:50:14 -0600

    removed unused files

--- mygame/app/tools/global_params.rb
+++ mygame/app/tools/global_params.rb
@@ -1,7 +1,7 @@
 module GlobalParams
     
     MAP_SCALE = 215 #The width of the map in miles
-    SPEED_FACTOR = 1/36 #Converts speed to nautical miles per second
+    SPEED_FACTOR = 1/3600 #Converts speed to nautical miles per second
     SCREEN_SIZE = 1280 #Size of standard game screen for Rubydragon
     SCALE_FACTOR = MAP_SCALE/SCREEN_SIZE #Converts miles to pixels
     VS_FACTOR = 1/60 #converts vspeed to ft/sec
