782ca888cb05b2c276906da1585599f41aac86d3
Ruben Bañuelos <rubenbanuelos@users.noreply.github.com>
2026-08-29 02:28:21 -0600

    populating aircraft

--- mygame/app/events.rb
+++ mygame/app/events.rb
@@ -1,7 +1,9 @@
 def add_events(s)
   s.events = {
+    5   => -> {@s.spawn_aicraft(@s.lobby["TRP3"])},
     60  => -> {@s.aircrafts["TRP3"].radar_target.squawk_ifr = true},
-    61  => -> {@s.alert_ident("TRP3")}
+    61  => -> {@s.alert_ident("TRP3")}, 
+    70  => -> {@s.aircrafts["QXE178"].assigned_heading = 90}
   }
   
 end
--- mygame/app/populate.rb
+++ mygame/app/populate.rb
@@ -3,15 +3,22 @@
 def populate(s)
 
 
-    s.add_aircraft("TRP3")
-    s.aircrafts["TRP3"].altitude = 6500
-    s.aircrafts["TRP3"].ias = 95
-    s.aircrafts["TRP3"].type = "B06T"
-    s.aircrafts["TRP3"].position = Position.new(202.40, 55.20)
-    s.aircrafts["TRP3"].heading = 295
-    s.aircrafts["TRP3"].assigned_altitude = 6500
-    s.aircrafts["TRP3"].assigned_waypoint = @s.waypoints["WGR"]
-    s.aircrafts["TRP3"].radar_target.squawk_ifr = false
+    s.add_aircraft_to_lobby("TRP3")
+    s.lobby["TRP3"].altitude = 6500
+    s.lobby["TRP3"].ias = 95
+    s.lobby["TRP3"].type = "B06T"
+    s.lobby["TRP3"].position = Position.new(202.40, 55.20)
+    s.lobby["TRP3"].heading = 295
+    s.lobby["TRP3"].assigned_waypoint = @s.waypoints["WGR"]
+    s.lobby["TRP3"].radar_target.squawk_ifr = false
+
+    s.add_aircraft("QXE178")
+    s.aircrafts["QXE178"].altitude = 500
+    s.aircrafts["QXE178"].ias = 170
+    s.aircrafts["QXE178"].type = "DH8A"
+    s.aircrafts["QXE178"].position = Position.new(167, 74)
+    s.aircrafts["QXE178"].heading = 20
+    s.aircrafts["QXE178"].assigned_altitude = 25000
 
 
 
