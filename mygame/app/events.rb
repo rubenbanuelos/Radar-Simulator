2aadd81f2840bab11c40c51fcb53f7cd5c6ab23b
Ruben Bañuelos <rubenbanuelos@users.noreply.github.com>
2026-08-30 23:28:14 -0600

    split events

--- mygame/app/events.rb
+++ mygame/app/events.rb
@@ -1,20 +1,55 @@
 def add_events(s)
-  s.events = {
-    #30   => -> {@s.aircrafts["ACA1754"].say_bearing_to(@s.waypoints["JOH"])},
-    #30   => -> {@s.aircrafts["ACA1754"].say_speed},
-    1   => -> {@s.spawn_aicraft(@s.lobby["RVV35"])},
-    5   => -> {@s.spawn_aicraft(@s.lobby["TRP3"])},
-    28  => -> {@s.aircrafts["RVV35"].assigned_altitude = 7000},
-    60  => -> {@s.aircrafts["TRP3"].radar_target.squawk_ifr = true},
-    61  => -> {@s.alert_ident("TRP3")},
-    62  => -> {@s.aircrafts["N721SQ"].say_position},
-    70  => -> {@s.aircrafts["QXE178"].assigned_waypoint = @s.waypoints["FORAT"]},
-    80  => -> {@s.spawn_aicraft(@s.lobby["ACA1754"])},
-    90  => -> {@s.aircrafts["ACA1754"].radar_target.in_control = true},
-    91  => -> {@s.alert_handover_rcv("ACA1754")},
+  
+  t = 146
 
-    395 => -> {@s.aircrafts["ACA1754"].assigned_waypoint = @s.waypoints["ANC"]},
-    460 => -> {@s.aircrafts["FDX792"].assigned_waypoint = @s.waypoints["JOVOM"]}
+  s.events["RVV35"] = {
+    t + 0   => -> {@s.spawn_aicraft(@s.lobby["RVV35"])},
+    t + 36  => -> {@s.aircrafts["RVV35"].assigned_altitude = 7000},
+    t + 100 => -> {@s.aircrafts["RVV35"].assigned_heading = 315},
+    t + 135 => -> {@s.alert_handover_snt("RVV35")},
+    t + 140 => -> {@s.aircrafts["RVV35"].assigned_heading = 16},
+    t + 150 => -> {@s.aircrafts["RVV35"].radar_target.in_control = false},
+    t + 254 => -> {@s.aircrafts.delete("RVV55")}
   }
   
+  t = 5
+
+  s.events["TRP3"] = {
+    t + 0   => -> {@s.spawn_aicraft(@s.lobby["TRP3"])},
+    t + 50  => -> {@s.aircrafts["TRP3"].radar_target.squawk_ifr = true}
+  }
+
+  s.events["TRP3 Alerts"] = {
+    t + 50  => -> {@s.alert_ident("TRP3")},
+  }
+
+  t = 80
+  
+  s.events["ACA1754"] = {
+    t + 0   => -> {@s.spawn_aicraft(@s.lobby["ACA1754"])},
+    t + 20  => -> {@s.aircrafts["ACA1754"].radar_target.in_control = true},
+    t + 315 => -> {@s.aircrafts["ACA1754"].assigned_waypoint = @s.waypoints["ANC"]}
+  }
+
+  s.events["ACA1754 Alerts"] = {
+    t + 20  => -> {@s.alert_handover_rcv("ACA1754")},
+  }
+
+  t = 0
+
+  s.events["N721SQ"] = {
+  }
+
+  t = 70
+
+  s.events["QXE178"] = {
+    t + 0   => -> {@s.aircrafts["QXE178"].assigned_waypoint = @s.waypoints["FORAT"]}
+  }
+
+  t = 460
+
+  s.events["FDX792"] = {
+    t + 0   => -> {@s.aircrafts["FDX792"].assigned_waypoint = @s.waypoints["JOVOM"]}
+  }
+
 end
